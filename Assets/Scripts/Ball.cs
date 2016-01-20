6559ff843cd7ad26dc7b8656b5fb134325d6f32c
SamaraRen <yren17@illinois.edu>
2016-01-19 22:52:42 -0600

    unfinished game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public bool inPlay = false;
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.useGravity =false;
+	}
+	
+	public void Launch (Vector3 velocity)
+	{
+		inPlay = true;
+		rigidBody.useGravity = true;
+		rigidBody.velocity = velocity;
+		
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play ();
+	}
+	
+}
+
---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+	
+	public float standingThreshold = 3f;
+	
+	public bool IsStanding () {
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+		float titltInX = Mathf.Abs (rotationInEuler.x);
+		float titltInZ = Mathf.Abs (rotationInEuler.z);
+		print (name+""+ titltInX +"   "+ titltInZ);
+		if (titltInX < standingThreshold && titltInZ < standingThreshold) {
+			return true;
+		} else {
+		return false;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+	IsStanding();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
